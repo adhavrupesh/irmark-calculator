258cc16828a43703f6a0c7deb64a384b6fce6704
Rupesh Adhav <adhavrupesh84@gmail.com>
2021-09-28 19:00:07 +0530

    first commit

--- src/main/java/com/example/paycaptainirmarkcalculator/DemoApplication.java
+++ src/main/java/com/example/paycaptainirmarkcalculator/DemoApplication.java
@@ -80,7 +80,7 @@ public class DemoApplication {
 	public String  sendepsxmlwithirmark(@RequestHeader HttpHeaders headers,
 										 @RequestBody com.example.paycaptainirmarkcalculator.eps.GovTalkMessage xmldata) throws Exception {
 
-		System.out.println("####monthnumber = "+ headers.get("monthnumber").get(0));
+		System.out.println("#### calculateeps monthnumber = "+ headers.get("monthnumber").get(0));
 
 		//String endDateMonth = xmldata.Body.IRenvelope.IRheader.PeriodEnd;
 		String taxYear = xmldata.Body.IRenvelope.EmployerPaymentSummary.RelatedTaxYear;
@@ -113,7 +113,7 @@ public class DemoApplication {
 
 		IRMarkCalculator mc = new IRMarkCalculator();
 		String base64 = mc.createMark(targetStream);
-		System.out.println("output base64 : "+base64);
+		System.out.println("#### calculateeps output base64 : "+base64);
 
 		if(base64 != "" && xmlContent.contains("<IRmark Type=\"generic\"></IRmark>")){
 			xmlContent = xmlContent.replace("<IRmark Type=\"generic\"></IRmark>", "<IRmark Type=\"generic\">"+base64+"</IRmark>");
