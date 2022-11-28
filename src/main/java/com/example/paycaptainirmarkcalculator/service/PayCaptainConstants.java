b9544131b2d84cb247ba39bef1e36113b6c5a82a
Rupesh Adhav <adhavrupesh84@gmail.com>
2022-11-28 15:37:27 +0530

    updated test functions 5

--- src/main/java/com/example/paycaptainirmarkcalculator/service/PayCaptainConstants.java
+++ src/main/java/com/example/paycaptainirmarkcalculator/service/PayCaptainConstants.java
@@ -1,7 +1,7 @@
 package com.example.paycaptainirmarkcalculator.service;
 
 public class PayCaptainConstants {
-    public static final String SANDBOX_WEBHOOK_URL = "https://paycaptain--fullcopy.sandbox.my.salesforce-sites.com/webhook/services/apexrest/v1/update/hmrcsubmission";
+    public static final String SANDBOX_WEBHOOK_URL = "https://paycaptain--dev.sandbox.my.salesforce-sites.com/webhook/services/apexrest/v1/update/hmrcsubmission";
     public static final String PRODUCTION_WEBHOOK_URL = "https://paycaptain.secure.force.com/webhook/services/apexrest/v1/update/hmrcsubmission";
     public static final String FULL_COPY_SANDBOX_WEBHOOK_URL = "https://paycaptain--fullcopy.sandbox.my.salesforce-sites.com/webhook/services/apexrest/v1/update/hmrcsubmission";
 }
