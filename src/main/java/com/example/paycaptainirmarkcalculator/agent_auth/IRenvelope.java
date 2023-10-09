73626cee6bd53533d14b39ac9031b8da10b081bb
Rupesh Adhav <adhavrupesh84@gmail.com>
2023-10-09 17:17:09 +0530

    added xmlns for IRenvelope

--- src/main/java/com/example/paycaptainirmarkcalculator/agent_auth/IRenvelope.java
+++ src/main/java/com/example/paycaptainirmarkcalculator/agent_auth/IRenvelope.java
@@ -1,15 +1,9 @@
 package com.example.paycaptainirmarkcalculator.agent_auth;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.XmlAttribute;
 
-@XmlType(namespace="http://www.govtalk.gov.uk/taxation/AgentAuthRequest/1")
+
 public class IRenvelope {
-
-    @XmlAttribute(name = "xmlns", namespace = "http://www.govtalk.gov.uk/taxation/AgentAuthRequest/1")
-    public String xmlns;
-
     @XmlElement
     public IRheader IRheader;
     @XmlElement
